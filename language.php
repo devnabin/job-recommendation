92e4f168c13526300e86ded5273e73c18a7e2eba
Your Name <nabin123456j@gmail.com>
2023-04-13 15:46:13 +0545

    uncommenting style.css

--- language.php
+++ language.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <title>My React App</title>
+    <script src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+      const Greeting = (props) => {
+        return <h1>Hello, {props.name}!</h1>;
+      }
 
-<body>
-    <?php
-    $nabin = 'nabin cash';
-    for ($a = 0; $a < 10; $a++)
-        echo '<div>' . $nabin . '</div>'
-
-    ?>
-</body>
-
+      ReactDOM.render(
+        <Greeting name="World" />,
+        document.getElementById('root')
+      );
+    </script>
+  </body>
 </html>
