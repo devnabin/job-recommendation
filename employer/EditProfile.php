d95a335c4e4992f29163dcf202ab05d1d73f71b2
Your Name <nabin123456j@gmail.com>
2023-05-04 13:19:24 +0545

    employeer done

--- employer/EditProfile.php
+++ employer/EditProfile.php
@@ -28,6 +28,10 @@ if (!isset($_SESSION['name'])) {
 
   <!-- Custom styles for this template -->
   <link href="./css/dashboard.css" rel="stylesheet">
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -102,97 +106,60 @@ if (!isset($_SESSION['name'])) {
         // Loop through each records 
         $row = mysqli_fetch_array($result)
         ?>
-        <form method="post" action="UpdateProfile.php">
-          <table width="100%" border="1" cellspacing="2" cellpadding="2">
-            <tr>
-              <td><strong>Company ID:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtId" type="text" id="txtId" value="<?php echo $row['Employer_Id']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Company Name:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtName" type="text" id="txtName" value="<?php echo $row['CompanyName']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Contact Person:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtContact" type="text" id="txtContact" value="<?php echo $row['ContactPerson']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Address:</strong></td>
-              <td><span>
-                  <label>
-                    <textarea name="txtAddress" id="txtAddress" cols="35" rows="3"><?php echo $row['Address']; ?></textarea>
-                  </label>
-                  <span class="textareaRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>City:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtCity" type="text" id="txtCity" value="<?php echo $row['City']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Email:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtEmail" type="text" id="txtEmail" value="<?php echo $row['Email']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Mobile:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtMobile" type="text" id="txtMobile" value="<?php echo $row['Mobile']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Area of Work:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtArea" type="text" id="txtArea" value="<?php echo $row['Area_Work']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>User Name:</strong></td>
-              <td><span id="sprytextfield8">
-                  <label>
-                    <input name="txtUser" type="text" id="txtUser" value="<?php echo $row['UserName']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-            <tr>
-              <td><strong>Password:</strong></td>
-              <td><span>
-                  <label>
-                    <input name="txtPassword" type="password" id="txtPassword" value="<?php echo $row['Password']; ?>" />
-                  </label>
-                  <span class="textfieldRequiredMsg">A value is required.</span></span></td>
-            </tr>
-
-            <tr>
-              <td>&nbsp;</td>
-              <td><label>
-                  <input type="submit" name="button" id="button" value="Update" />
-                </label></td>
-            </tr>
-          </table>
-        </form>
+        <form method="post" action="UpdateProfile.php" class="bg-dark-subtle p-5">
+          <div class="form-group">
+            <label for="txtId">Company ID:</label>
+            <input name="txtId" type="text" id="txtId" class="form-control" value="<?php echo $row['Employer_Id']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtName">Company Name:</label>
+            <input name="txtName" type="text" id="txtName" class="form-control" value="<?php echo $row['CompanyName']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtContact">Contact Person:</label>
+            <input name="txtContact" type="text" id="txtContact" class="form-control" value="<?php echo $row['ContactPerson']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtAddress">Address:</label>
+            <textarea name="txtAddress" id="txtAddress" class="form-control" cols="35" rows="3"><?php echo $row['Address']; ?></textarea>
+            <span class="textareaRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtCity">City:</label>
+            <input name="txtCity" type="text" id="txtCity" class="form-control" value="<?php echo $row['City']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtEmail">Email:</label>
+            <input name="txtEmail" type="text" id="txtEmail" class="form-control" value="<?php echo $row['Email']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtMobile">Mobile:</label>
+            <input name="txtMobile" type="text" id="txtMobile" class="form-control" value="<?php echo $row['Mobile']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtArea">Area of Work:</label>
+            <input name="txtArea" type="text" id="txtArea" class="form-control" value="<?php echo $row['Area_Work']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtUser">User Name:</label>
+            <input name="txtUser" type="text" id="txtUser" class="form-control" value="<?php echo $row['UserName']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="form-group">
+            <label for="txtPassword">Password:</label>
+            <input name="txtPassword" type="password" id="txtPassword" class="form-control" value="<?php echo $row['Password']; ?>" />
+            <span class="textfieldRequiredMsg">A value is required.</span>
+          </div>
+          <div class="text-center">
+            <input type="submit" class="btn btn-primary" name="button" id="button" value="Update" />
+          </div>
 
       </main>
     </div>
